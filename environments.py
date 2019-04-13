b01ca886adc3b199e6fc24fd48b582250c488a49
lgulich <lgulich@ethz.ch>
2019-04-14 01:52:07 +0200

    add new envs

--- environments.py
+++ environments.py
@@ -20,7 +20,10 @@ class EnvironmentBase(object):
         self.light = light
         self.transitions = {
             'good_guy_entering': self.good_guy_entering,
-            'bad_guy_entering': self.bad_guy_entering
+            'bad_guy_entering': self.bad_guy_entering,
+            'leaving_house': self.leaving_house,
+            'requesting_dance_mode': self.requesting_dance_mode,
+            'requesting_romantic_mode': self.requesting_romantic_mode
         }
 
     def activate_environment(self, name):
@@ -48,6 +51,21 @@ class EnvironmentBase(object):
         print('bg entering THIS SHOULD NEVER BE CALLED')
         pass
 
+    @abstractmethod
+    def leaving_house(self):
+        print('THIS SHOULD NEVER BE CALLED')
+        pass
+
+    @abstractmethod
+    def requesting_dance_mode(self):
+        print('THIS SHOULD NEVER BE CALLED')
+        pass
+
+    @abstractmethod
+    def requesting_romantic_mode(self):
+        print('THIS SHOULD NEVER BE CALLED')
+        pass
+
 
 class EnvironmentOff(EnvironmentBase):
     """
@@ -63,7 +81,16 @@ class EnvironmentOff(EnvironmentBase):
 
     def bad_guy_entering(self):
         # turn hue on and red
-        return self.activate_environment('intruder')
+        return self.activate_environment('off')
+
+    def leaving_house(self):
+        return self.activate_environment('alarm')
+
+    def requesting_dance_mode(self):
+        return self.activate_environment('dance')
+
+    def requesting_romantic_mode(self):
+        return self.activate_environment('romantic')
 
 
 class EnvironmentNormal(EnvironmentBase):
@@ -80,7 +107,16 @@ class EnvironmentNormal(EnvironmentBase):
 
     def bad_guy_entering(self):
         # turn hue on and red
-        return self.activate_environment('intruder')
+        return self.activate_environment('normal')
+
+    def leaving_house(self):
+        return self.activate_environment('alarm')
+
+    def requesting_dance_mode(self):
+        return self.activate_environment('dance')
+
+    def requesting_romantic_mode(self):
+        return self.activate_environment('romantic')
 
 
 class EnvironmentIntruder(EnvironmentBase):
@@ -98,3 +134,64 @@ class EnvironmentIntruder(EnvironmentBase):
     def bad_guy_entering(self):
         # turn hue on and red
         return self.activate_environment('intruder')
+
+    def leaving_house(self):
+        return self.activate_environment('alarm')
+
+    def requesting_dance_mode(self):
+        return self.activate_environment('dance')
+
+    def requesting_romantic_mode(self):
+        return self.activate_environment('romantic')
+
+
+class EnvironmentDance(EnvironmentBase):
+    """
+    the intruder environment (is active when a non authorized person is in the room)
+    """
+
+    def __init__(self, light):
+        super(EnvironmentDance, self).__init__('dance', light)
+
+    def good_guy_entering(self):
+        # turn hue on and green
+        return self.activate_environment('dance')
+
+    def bad_guy_entering(self):
+        # turn hue on and red
+        return self.activate_environment('dance')
+
+    def leaving_house(self):
+        return self.activate_environment('alarm')
+
+    def requesting_dance_mode(self):
+        return self.activate_environment('dance')
+
+    def requesting_romantic_mode(self):
+        return self.activate_environment('romantic')
+
+
+class EnvironmentRomantic(EnvironmentBase):
+    """
+    the intruder environment (is active when a non authorized person is in the room)
+    """
+
+    def __init__(self, light):
+        super(EnvironmentRomantic, self).__init__('romantic', light)
+
+    def good_guy_entering(self):
+        # turn hue on and green
+        return self.activate_environment('intruder')
+
+    def bad_guy_entering(self):
+        # turn hue on and red
+        return self.activate_environment('intruder')
+
+    def leaving_house(self):
+        return self.activate_environment('alarm')
+
+    def requesting_dance_mode(self):
+        return self.activate_environment('dance')
+
+    def requesting_romantic_mode(self):
+        return self.activate_environment('romantic')
--- events.py
+++ events.py
@@ -4,8 +4,14 @@ from enum import Enum
 class Events(Enum):
     """
     List of events
-    - good_guy_entering
-    - bad_guy_entering
+    - good_guy_entering = 1
+    - bad_guy_entering = 2
+    - leaving_house = 3
+    - requesting_dance_mode = 4
+    - requesting_romantic_mode = 5
     """
     good_guy_entering = 1
     bad_guy_entering = 2
+    leaving_house = 3
+    requesting_dance_mode = 4
+    requesting_romantic_mode = 5
